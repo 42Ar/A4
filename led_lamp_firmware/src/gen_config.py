128a9ede74e66303b4f633e1d96b677fd1dee4b9
Frank Hermann <alphargon@gmail.com>
2022-11-01 23:17:41 +0100

    add lemon light config gen

--- led_lamp_firmware/src/gen_config.py
+++ led_lamp_firmware/src/gen_config.py
@@ -6,3 +6,7 @@ for i in range(1, 7):
     b["name"] = f"Lamp {i}"
     b["device_id"] = f"main-lamp-{i}"
     json.dump(b, open(f"config-{i}.json", "w"), indent=4)
+b["wifi"]["ip"] = f"192.168.1.40"
+b["name"] = f"lemon-light"
+b["device_id"] = f"lemon-light"
+json.dump(b, open(f"config-lemon-light.json", "w"), indent=4)
